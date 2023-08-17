ebbac01cc15d58f6c9a0064fbfc1ca4fa0a80d31
Hari05Malliga <hari.malliga@gmail.com>
2023-08-17 22:48:36 +0530

    Program to open multiple files and check the proc/pid/fd diresctory

---
+++ Program_5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main() {
+	int fd1 = open ( "file1", O_CREAT, 0744);
+	int fd2 = open ( "file2", O_CREAT, 0744);
+	int fd3 = open ( "file3", O_CREAT, 0744);
+	int fd4 = open ( "file4", O_CREAT, 0744);
+	int fd5 = open ( "file5", O_CREAT, 0744);
+
+	while(1);
+
+	close(fd1);
+	close(fd2);
+	close(fd3);
+	close(fd4);
+	close(fd5);
+	return 0;
+}
