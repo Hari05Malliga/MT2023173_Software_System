b5d05e45579da4c3bdd28e4bf2d22824b81e7433
Hari05Malliga <hari.malliga@gmail.com>
2023-08-17 22:27:30 +0530

    Program for creat system call

---
+++ Program_3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main() {
+	int fd = creat ( "new_file", 0744);
+	if ( fd == -1 ) {
+		perror ("File Creation failed...");
+		return fd;
+	}
+	printf ("File Descriptor value : %d\n", fd);
+	close(fd);
+	return 0;
+}
